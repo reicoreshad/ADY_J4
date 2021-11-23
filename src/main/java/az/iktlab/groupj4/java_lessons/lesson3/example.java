26a1ac2ff82af9a57c8b9559a3435565792f529b
Rəşad Əskərov <r.askerov@local.ady.az>
2021-11-23 20:29:03 +0400

    Initial commit

---
+++ src/main/java/az/iktlab/groupj4/java_lessons/lesson3/example.java
@@ -0,0 +1,18 @@
+package az.iktlab.groupj4.java_lessons.lesson3;
+
+public class example {
+    public static void main (String[] args) {
+        int num1=15;
+        int num2=30;
+        num1=num2-num1;
+        num2=num2-num1;
+        num1=num1+num2;
+
+        System.out.println(num1);
+        System.out.println(num2);
+        float flt=2.5f;
+        double x=23.65d;
+
+
+    }
+}
