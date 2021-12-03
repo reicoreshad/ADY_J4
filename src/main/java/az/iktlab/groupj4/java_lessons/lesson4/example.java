64c637f0399b37371771f23d47920ea89d8a4c07
r.askerov <defero.askerov@gmail.com>
2021-12-03 19:05:37 +0400

    lesson 5

--- src/main/java/az/iktlab/groupj4/java_lessons/lesson4/example.java
+++ src/main/java/az/iktlab/groupj4/java_lessons/lesson4/example.java
@@ -19,11 +19,40 @@ public class example {
                     //System.out.
 //            }
 //        }
-        Scanner scn=new Scanner(System.in);
-        String val=scn.next();
+        //Scanner scn=new Scanner(System.in);
+        //String val=scn.next();
         //val=val.replace("x","");
-        val=val.replace("x"," ").trim().replace(" ","x");
-        System.out.println( val);
+        //val=val.replace("x"," ").trim().replace(" ","x");
+        //System.out.println( val);
+
+        String val;
+        int firsttwo,lasttwo;
+        for (int i = 1000; i <=10000 ; i++) {
+            val=String.valueOf(i);
+            firsttwo=Integer.valueOf(val.charAt(0))+Integer.valueOf(val.charAt(1));
+            lasttwo=Integer.valueOf(val.charAt(val.length()-1))+Integer.valueOf(val.charAt(val.length()-2));
+            if(firsttwo==lasttwo) System.out.println(i);
+        }
 
     }
 }
+//public class main{
+//   public static void main(String[]args){
+//      Scanner sc= new Scanner(System.in);
+//        String letter="";
+//       int num,pos=0,neg=0,zer=0;
+//        while(! letter.equals("exit"))
+//        {
+//           letter = sc.next();
+//
+//           if(letter.matches("-[0-9]")){
+//               num=Integer.valueOf(letter);
+//                if(num>0) pos++; else if(num<0) neg++; else zer++;
+//           }
+//        }
+//        System.out.println("positive number counts is: "+pos);
+//        System.out.println("negative number counts is: "+neg);
+//        System.out.println("zero number counts is: "+zer);
+
+//    }
+//}
